367b7bb96e407b810c735d7ce1eef4fd8531dfce
Vladimir <vladimir@zontar.ru>
2013-09-23 01:51:17 +0700

    fix filling bd

--- BDMaker/bdmaker.cpp
+++ BDMaker/bdmaker.cpp
@@ -1,5 +1,6 @@
 #include "bdmaker.h"
 #include "ui_bdmaker.h"
+
 #include <QDebug>
 
 BDMaker::BDMaker(QWidget *parent) :
@@ -8,7 +9,7 @@ BDMaker::BDMaker(QWidget *parent) :
 {
     ui->setupUi(this);
     userParser.parse();
- //   createBD();
+//    createBD();
 }
 
 BDMaker::~BDMaker()
@@ -27,16 +28,16 @@ void BDMaker::createBD()
     }
     qDebug() << "successfully open db.sqlite";
     QSqlQuery query;
-    qDebug() << query.exec("CREATE TABLE \"Friends\" (\"key\" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, \"who\" INTEGER, \"whom\" INTEGER)");
-    qDebug() << query.exec("CREATE TABLE \"Likes\" (\"key\" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, \"user\" INTEGER, \"post\" INTEGER)");
-    qDebug() << query.exec("CREATE TABLE \"Posts\" (\"key\" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, \"id\" INTEGER UNIQUE, \"author\" INTEGER, \"thread\" INTEGER, \"time\" INTEGER, \"likes\" INTEGER, \"text\" TEXT)");
-    qDebug() << query.exec("CREATE TABLE \"Sections\" (\"key\" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, \"id\" INTEGER UNIQUE, \"name\" TEXT, \"parent\" INTEGER, \"threads\" INTEGER)");
-    qDebug() << query.exec("CREATE TABLE \"Threads\" (\"key\" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, \"id\" INTEGER UNIQUE, \"name\" TEXT, \"section\" INTEGER, \"author\" TEXT, \"create\" INTEGER, \"posts\" INTEGER)");
-    qDebug() << query.exec("CREATE TABLE \"Users\" (\"key\" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, \"id\" INTEGER UNIQUE, \"nickname\" TEXT, \"create\" INTEGER, \"birth\" INTEGER, \"views\" INTEGER, \"likesput\" INTEGER, \"likesgot\" INTEGER, \"posts\" INTEGER, \"prd\" REAL, \"lastactivity\" INTEGER)");
-    qDebug() << query.exec("CREATE INDEX \"PostIdIdx\" ON \"Posts\" (\"id\")");
-    qDebug() << query.exec("CREATE INDEX \"SectionsIdIdx\" ON \"Sections\" (\"id\")");
-    qDebug() << query.exec("CREATE INDEX \"ThreadIdIdx\" ON \"Threads\" (\"id\")");
-    qDebug() << query.exec("CREATE INDEX \"ThreadNameIdx\" ON \"Threads\" (\"name\")");
-    qDebug() << query.exec("CREATE INDEX \"UsersIdIdx\" ON \"Users\" (\"id\")");
+        query.exec("CREATE TABLE \"Likes\" (\"key\" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, \"user\" INTEGER, \"post\" INTEGER)");
+        query.exec("CREATE TABLE \"Posts\" (\"key\" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, \"id\" INTEGER UNIQUE, \"author\" INTEGER, \"thread\" INTEGER, \"time\" INTEGER, \"likes\" INTEGER, \"text\" TEXT)");
+        query.exec("CREATE TABLE \"Sections\" (\"key\" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, \"id\" INTEGER UNIQUE, \"name\" TEXT, \"parent\" INTEGER, \"threads\" INTEGER)");
+        query.exec("CREATE TABLE \"Threads\" (\"key\" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, \"id\" INTEGER UNIQUE, \"name\" TEXT, \"section\" INTEGER, \"author\" TEXT, \"create\" INTEGER, \"posts\" INTEGER)");
+        query.exec("CREATE TABLE \"Users\" (\"key\" INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, \"id\" INTEGER UNIQUE, \"nickname\" TEXT, \"create\" INTEGER, \"views\" INTEGER, \"likeput\" INTEGER, \"likegot\" INTEGER, \"posts\" INTEGER, \"ppd\" REAL, \"lastactivity\" INTEGER)");
+        query.exec("CREATE INDEX \"PostIdIdx\" ON \"Posts\" (\"id\")");
+        query.exec("CREATE INDEX \"SectionsIdIdx\" ON \"Sections\" (\"id\")");
+        query.exec("CREATE INDEX \"ThreadIdIdx\" ON \"Threads\" (\"id\")");
+        query.exec("CREATE INDEX \"ThreadNameIdx\" ON \"Threads\" (\"name\")");
+        query.exec("CREATE INDEX \"UsersIdIdx\" ON \"Users\" (\"id\")");
+        query.exec("CREATE INDEX \"UsersNicknameIdx\" ON \"Users\" (\"nickname\")");
     db.close();
 }
--- BDMaker/userparser.cpp
+++ BDMaker/userparser.cpp
@@ -10,78 +10,111 @@ UserParser::UserParser(QObject *parent) :
 {
     db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("db.sqlite");
+
+}
+
+void UserParser::parse()
+{
     if(!db.open())
     {
         qDebug() << "cannot open db.sqlite";
         return;
     }
     qDebug() << "successfully open db.sqlite";
-}
-
-void UserParser::parse()
-{
     QDir storage(QDir::currentPath());
     storage.cd("users storage");
     foreach(QString dir, storage.entryList(QDir::Dirs | QDir::NoDotAndDotDot))
     {
-        qDebug() << "\tsubdir:" << storage.absoluteFilePath(dir);
         QDir subdir(storage.absoluteFilePath(dir));
         foreach(QString file, subdir.entryList(QDir::Files))
         {
-            qDebug() << "\t\tfile:" << subdir.absoluteFilePath(file);
             parseFile(subdir.absoluteFilePath(file));
         }
     }
+    db.close();
 }
 
 void UserParser::addUser(UserInfo user)
 {
     QSqlQuery query;
-    query.exec("INSERT INTO \"Users\" ( \"id\",\"nickname\",\"create\",\"views\",\"likeput\",\"likegot\",\"posts\",\"ppd\",\"lastactivity\" ) \
-               VALUES ( '"+QString::number(user.id)+"',\
-                        '"+user.nickname+"',\
-                        '"+QString::number(user.createtime.toTime_t())+"',\
-                        '"+QString::number(user.views)+"',\
-                        '"+QString::number(user.likesput)+"',\
-                        '"+QString::number(user.likesgot)+"',\
-                        '"+QString::number(user.posts)+"',\
-                        '"+QString::number(user.ppd)+"',\
-                        '"+QString::number(user.lastactivity.toTime_t())+"' )");
+    qDebug() << query.exec("INSERT INTO \"Users\" ( \"id\",\"nickname\",\"create\",\"views\",\"likeput\",\"likegot\",\"posts\",\"ppd\",\"lastactivity\" ) \
+VALUES ( "+QString::number(user.id)+",\
+'"+user.nickname+"',\
+'"+QString::number(user.createtime.toTime_t())+"',\
+'"+QString::number(user.views)+"',\
+'"+QString::number(user.likesput)+"',\
+'"+QString::number(user.likesgot)+"',\
+'"+QString::number(user.posts)+"',\
+'"+QString::number(user.ppd)+"',\
+'"+QString::number(user.lastactivity.toTime_t())+"' )");
 }
 
 void UserParser::parseFile(const QString &fileName)
 {
+    // read file
     QFile file(fileName);
     file.open(QIODevice::ReadOnly);
-  //  QByteArray ba = file.readAll();
     QString data = QString::fromLocal8Bit(file.readAll());
     file.close();
+    // !read file
+
     QRegExp reg;
     reg.setPatternSyntax(QRegExp::RegExp2);
     reg.setMinimal(true);
     int pos=0;
+    UserInfo user;
+
     reg.setPattern("\"username_box\".*>(.*)</font>");
     pos = reg.indexIn(data,pos+1);
-    qDebug() << pos << reg.cap(1);
+    user.nickname = reg.cap(1);
+
     reg.setPattern("Всего сообщений:</span>\\s(.*)</li>");
     pos = reg.indexIn(data, pos+1);
-    qDebug() << pos << reg.cap(1);
+    if(pos!=-1) user.posts = reg.cap(1).replace(',',"").toInt();
+    else user.posts = 0;
+
     reg.setPattern("Сообщений в день:</span>\\s(.*)</li>");
     pos = reg.indexIn(data,pos+1);
-    qDebug() << pos << reg.cap(1);
-    reg.setPattern("Всего благодарностей:</span>\\s(\\d*)</li>");
+    if(pos!=-1) user.ppd = reg.cap(1).toDouble();
+    else user.ppd = 0;
+
+    reg.setPattern("Всего благодарностей:</span>\\s([,\\d]*)</li>");
     pos = reg.indexIn(data,pos+1);
-    qDebug() << pos << reg.cap(1);
-    reg.setPattern("Спасибо:\\s(\\d*)\\s");
+    if(pos!=-1) user.likesput = reg.cap(1).replace(',',"").toInt();
+    else user.likesput = 0;
+
+    reg.setPattern("Спасибо:\\s([,\\d]*)\\s");
     pos = reg.indexIn(data,pos+1);
-    qDebug() << pos << reg.cap(1);
+    if(pos!=-1) user.likesgot = reg.cap(1).replace(',',"").toInt();
+    else user.likesput = 0;
+
     reg.setPattern("Последняя активность:</span>\\s(.*)\\s<span class=\"time\">(.*)</span>");
     pos = reg.indexIn(data,pos+1);
-    qDebug() << pos << reg.cap(1);
+    if(pos!=-1)
+    {
+        QString ds = reg.cap(1);
+        QString ts = reg.cap(2);
+        if(ds=="Сегодня") ds = "22.09.2013";
+        else if(ds=="Вчера") ds = "21.09.2013";
+        QDateTime dt = QDateTime::fromString(ds+" "+ts,"dd.MM.yyyy hh:mm");
+        user.lastactivity = dt;
+    }
+    else user.lastactivity = QDateTime::fromTime_t(0);
+
     reg.setPattern("Регистрация:</span>\\s(.*)</li>");
     pos = reg.indexIn(data,pos+1);
-    qDebug() << pos << reg.cap(1);
-    reg.setPattern("Реферралы:</span>\\s(\\d*)<");
+    if(pos!=-1)
+    {
+        QDateTime dt = QDateTime::fromString(reg.cap(1),"dd.MM.yyyy");
+        user.createtime = dt;
+    }
+    else user.createtime = QDateTime::fromTime_t(0);
+
+    reg.setPattern("была посещена <strong>([,\\d]*)</strong>");
     pos = reg.indexIn(data,pos+1);
-    qDebug() << pos << reg.cap(1);
+    if(pos!=-1) user.views = reg.cap(1).replace(',',"").toInt();
+    else user.views = 0;
+
+    user.id = fileName.section('/',-1).toInt();
+    addUser(user);
 }
